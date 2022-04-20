4475407a302b120fd529425dcd94237d330befc6
pbartashov <p.bartashov@icloud.com>
2022-04-20 22:51:58 +1000

    Initial Commit

---
+++ CarAlarmControl/CarAlarmControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAlarmControlApp.swift
+//  CarAlarmControl
+//
+//  Created by Павел Барташов on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CarAlarmControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
